f598d3f56ae9526f222f2cac3f1ea7d4c5fe7f97
Hala Shahroor <shahroorhala@gmail.com>
2025-08-11 17:29:39 +0300

    version 7

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var navbar = document.querySelector(".navbar");
 window.onscroll=function(e){
-    console.log(window.scrollY)
-
+    
     if(window.scrollY>= 200){
     navbar.classList.add('navbar-scroll');
     
